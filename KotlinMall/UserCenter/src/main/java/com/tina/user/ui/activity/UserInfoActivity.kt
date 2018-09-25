498a652dbb0b4e79c2df8f124e081096e17f1eef
yxc <yxc@meitu.com>
2018-09-25 20:07:54 +0800

    1.抽象优化头像图片选择界面

--- KotlinMall/UserCenter/src/main/java/com/tina/user/ui/activity/UserInfoActivity.kt
+++ KotlinMall/UserCenter/src/main/java/com/tina/user/ui/activity/UserInfoActivity.kt
@@ -1,23 +1,14 @@
 package com.tina.user.ui.activity
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import android.util.Log
-import com.bigkoo.alertview.AlertView
-import com.bigkoo.alertview.OnItemClickListener
-import com.jph.takephoto.app.TakePhoto
-import com.jph.takephoto.app.TakePhotoImpl
-import com.jph.takephoto.compress.CompressConfig
 import com.jph.takephoto.model.TResult
-import com.kotlin.base.utils.DateUtils
 import com.qiniu.android.http.ResponseInfo
 import com.qiniu.android.storage.UpCompletionHandler
 import com.qiniu.android.storage.UploadManager
 import com.tina.base.common.BaseConstant
 import com.tina.base.ext.onClick
-import com.tina.base.ui.activity.BaseMvpActivity
+import com.tina.base.ui.activity.BaseTakePhotoActivity
 import com.tina.base.utils.AppPrefsUtils
 import com.tina.base.utils.GlideUtils
 import com.tina.provider.common.ProviderConstant
@@ -31,17 +22,11 @@ import com.tina.user.utils.UserPrefsUtils
 import kotlinx.android.synthetic.main.activity_user_info.*
 import org.jetbrains.anko.toast
 import org.json.JSONObject
-import java.io.File
 
 /**
  * 用户信息
  */
-class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView, TakePhoto.TakeResultListener {
-
-
-    private lateinit var mTakePhoto: TakePhoto
-
-    private lateinit var mTempFile: File
+class UserInfoActivity : BaseTakePhotoActivity<UserInfoPresenter>(), UserInfoView{
 
     private val mUPloadManager: UploadManager by lazy { UploadManager() }
 
@@ -58,8 +43,6 @@ class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView, Tak
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
-        mTakePhoto = TakePhotoImpl(this, this)
-        mTakePhoto.onCreate(savedInstanceState)
         injectComponent()
         initView()
         initData()
@@ -105,27 +88,6 @@ class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView, Tak
         }
     }
 
-
-    /*
-       弹出选择框，默认实现
-       可根据实际情况，自行修改
-    */
-    protected fun showAlertView() {
-        AlertView("选择图片", "", "取消", null, arrayOf("拍照", "相册"), this,
-                AlertView.Style.ActionSheet, OnItemClickListener { o, position ->
-            mTakePhoto.onEnableCompress(CompressConfig.ofDefaultConfig(), false)
-            when (position) {
-                0 -> {
-                    createTempFile()
-                    mTakePhoto.onPickFromCapture(Uri.fromFile(mTempFile))
-                }
-                1 -> mTakePhoto.onPickFromGallery()
-            }
-        }
-
-        ).show()
-    }
-
     override fun injectComponent() {
         DaggerUserComponent.builder().activityComponent(mActivityComponent)
                 .userModule(UserModule()).build().inject(this)
@@ -133,35 +95,8 @@ class UserInfoActivity : BaseMvpActivity<UserInfoPresenter>(), UserInfoView, Tak
     }
 
     override fun takeSuccess(result: TResult?) {
-        Log.d("TakePhoto", result?.image?.originalPath)
-        Log.d("TakePhoto", result?.image?.compressPath)
-
         mLocalFileUrl = result?.image?.compressPath
-
         mPresenter.getUploadToken()
-    }
-
-    override fun takeCancel() {
-
-    }
-
-    override fun takeFail(result: TResult?, msg: String?) {
-
-        Log.e("TakePhoto", msg)
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        mTakePhoto.onActivityResult(requestCode, resultCode, data)
-    }
-
-    fun createTempFile() {
-        val tempFileName = "${DateUtils.curTime}.png"
-        if (Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
-            this.mTempFile = File(Environment.getExternalStorageDirectory(), tempFileName)
-            return
-        }
-        this.mTempFile = File(filesDir, tempFileName)
     }
 
 
